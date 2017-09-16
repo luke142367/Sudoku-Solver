255a5c1a42941f6cc73baa8bdc9030fccdd79215
luke142367 <lukebenjaminely@gmail.com>
2017-09-16 22:10:08 +0100

    Initial commit - setting up the window

---
+++ src/Main/Window.java
@@ -0,0 +1,24 @@
+package Main;
+
+import javax.swing.*;
+
+/**
+ * Created by LUKE_2 on 31/10/2015.
+ */
+public class Window extends JFrame{
+
+    public static void main(String[] args){
+        new Window();
+    }
+
+    public Window(){
+        super("Sudoku Solver");
+        this.setContentPane(new WindowPanel());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+}
---
+++ src/Main/WindowPanel.java
@@ -0,0 +1,120 @@
+package Main;
+
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.awt.peer.MouseInfoPeer;
+
+/**
+ * Created by LUKE_2 on 31/10/2015.
+ */
+public class WindowPanel extends JPanel implements Runnable, MouseListener, MouseMotionListener{
+    private int Height = 800, Width = 800;
+    private boolean running;
+    private Thread thread;
+    private int targetTps = 60;
+
+    private BufferedImage image;
+    private Graphics2D g;
+
+    public WindowPanel() {
+        setPreferredSize(new Dimension(Width, Height));
+        setFocusable(true);
+        requestFocus();
+        init();
+    }
+
+    private void init() {
+        //gsm = new GameStateManager(Width, Height);
+        image = new BufferedImage(Width, Height, BufferedImage.TYPE_INT_ARGB);
+        running = true;
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        g = (Graphics2D)image.getGraphics();
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    private void update(){
+
+    }
+
+    private void draw(){
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,Width,Height);
+
+    }
+
+
+    public void run() {
+        int fps = 0; int tick = 0;
+        double fpsTimer = System.currentTimeMillis();
+        double secondsPerTick = 1D / targetTps;
+        double nsPerTick = secondsPerTick * 1000000000D;
+        double then = System.nanoTime();
+        double now;
+        double unprocessed = 0;
+        while(running){
+            now = System.nanoTime();
+            unprocessed += (now - then) ;
+            then = now;
+            while(unprocessed >= nsPerTick){
+                update();
+                tick++;
+                unprocessed -= nsPerTick;
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            repaint();
+            fps++;
+            if(System.currentTimeMillis() - fpsTimer >= 1000){
+                //game.setTitle(tick,fps,mean);
+//                System.out.println("Fps: " + fps + " Tps: " + tick);
+                fps = 0;
+                tick = 0;
+                fpsTimer += 1000;
+            }
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, Width, Height);
+        draw();
+        g.drawImage(image, 0, 0, Width, Height, null);
+    }
+
+    public void mouseClicked(MouseEvent e){
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
